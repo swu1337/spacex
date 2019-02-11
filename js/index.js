902066b7bc4b46cd7bd9d8512f1e11f29e4ed04c
swu1337 <master_msp1@hotmail.com>
2019-02-11 16:18:00 +0100

    navigation for mobile done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 window.onload = () => {
     console.log(`Test`);
+
+    document.querySelector(".menu").onclick = () => {
+        let header = document.querySelector("header");
+        console.log(header);
+        let icn = document.querySelector(".icn-menu");
+
+        if(header.classList.contains("open")) {
+            header.classList.remove("open");
+            icn.classList.remove('fa-times');
+            icn.classList.add('fa-bars');
+        } else {
+            header.classList.add("open");
+            icn.classList.remove('fa-bars');
+            icn.classList.add('fa-times');
+        }
+    }
 }
