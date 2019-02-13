04b900500efd29fc120492e1923ba10eb5b58689
swu1337 <master_msp1@hotmail.com>
2019-02-13 13:55:31 +0100

    finish on board.started full mobile

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
-let ctx = document.getElementById('onboard-canvas').getContext('2d');
+let onboardCtx = document.getElementById('onboard-canvas').getContext('2d');
+let fuelCtx = document.getElementById('fuel-canvas').getContext('2d');
 
-let myPieChart = new Chart(ctx,{
+let onboardPie = new Chart(onboardCtx, {
     type: 'pie',
     data: {
         datasets: [{
@@ -10,8 +11,6 @@ let myPieChart = new Chart(ctx,{
                 'rgba(17, 92, 54, 1)'
             ]
         }],
-
-        // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
             'Crew',
             'Passengers'
@@ -25,6 +24,34 @@ let myPieChart = new Chart(ctx,{
                 top: 10,
                 bottom: 10
             }
-        }
+        },
+        maintainAspectRatio: true
+    }
+});
+let fuelPie = new Chart(fuelCtx, {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [90, 10],
+            backgroundColor: [
+                'rgba(207, 92, 54, 1)',
+                'rgba(17, 92, 54, 0)'
+            ]
+        }],
+        labels: [
+            'Fuel',
+            'Empty'
+        ]
+    },
+    options: {
+        layout: {
+            padding: {
+                left: 0,
+                right: 0,
+                top: 10,
+                bottom: 10
+            }
+        },
+        maintainAspectRatio: true
     }
 });
